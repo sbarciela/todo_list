a18910da6c4f24017982089c6d5657903f5cb15c
sebastian barciela <barcielasebastian@gmail.com>
2023-08-30 11:08:08 -0300

    cambiando los iconos a imagenes

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,6 +1,10 @@
 import DeleteIcon from "./assets/delete_outline.svg"
 import Checkbox from "./assets/check_box_outline_blank.svg"
 import CheckboxFilled from "./assets/check_box.svg";
+import check from "./assets/check_box_outline_blank.svg?url"
+import checkboxFilled from "./assets/check_box.svg?url";
+import deleteIcon from "./assets/delete_outline.svg?url"
+
 
 
 function Task (props){
@@ -26,15 +30,16 @@ props.active?inputClass="":inputClass="task-done";
 
     return(
         <div >
+            
            <form action="get" onSubmit={deleteTask} className="task">
             {props.active?
-             <Checkbox className="checkbox" onClick={toggleTasks} />
+             <img src={check} className="checkbox" onClick={toggleTasks} alt="checkbox"/>
              :
-             <CheckboxFilled className="checked" onClick={toggleTasks} />
+             <img src={checkboxFilled} className="checked" onClick={toggleTasks} alt="checkboxfilled" />
             }
                       
            <span onClick={toggleTasks} className={inputClass} >{props.name}</span>
-            {props.activeFilter==="Completed"?<DeleteIcon className="delete-icon" onClick={deleteTask}/>:null}
+            {props.activeFilter==="Completed"?<img src={deleteIcon} className="delete-icon" onClick={deleteTask} alt="delete"/>:null}
            </form>
         </div>
     )
